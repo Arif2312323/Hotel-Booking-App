e36b9cd1a7b6b7df6b9e7b1b1b3e0f71dda0d575
Arif2312323 <arifalmas3.14@gmail.com>
2024-05-23 02:57:26 +0530

    Developed the Add hotel Feature

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -7,12 +7,19 @@ import userAuth from "./routes/auth"
 import morgan from "morgan";
 import cookieparser from "cookie-parser";
 import path from "path";
+import {v2 as cloudinary} from 'cloudinary'
+import myhotelsroutes from "./routes/my-hotels"
 mongoose.connect(process.env.MONGO_CONNECTION_URL as string);
 
 const app = express();
 app.use(cookieparser());
 app.use(express.json());
 
+cloudinary.config({
+  cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+  api_key : process.env.CLOUDINARY_API_KEY,
+  api_secret : process.env.CLOUDINARY_API_SECRET
+});
 app.use(express.urlencoded({extended: true}));
 app.use(cors(
   {
@@ -20,10 +27,10 @@ app.use(cors(
     credentials : true
   }
 ));
-
 app.use(express.static(path.join(__dirname,"../../Frontend/dist")));
 app.use("/api/users", userRoutes);
 app.use("/api/users",userAuth);
+app.use("/api/my-hotels",myhotelsroutes);
 app.listen(3000,()=>{
     console.log("Server Started")
 });
